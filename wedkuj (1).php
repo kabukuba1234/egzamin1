2a2c813039ff8e06a3c09210e4c3ed9212be2659
kabukuba1234 <kabukuba@wp.pl>
2021-09-07 17:29:52 +0200

    Create wedkuj (1).php

---
+++ wedkuj (1).php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wędkujemy</title>
+    <link rel="stylesheet" href="styl_1.css">
+</head>
+<body>
+    <header>
+    <h1>Portal dla wędkarzy</h1>
+    <div id="lewy">
+        <h2>Ryby drapieżne naszych wód</h2>
+       
+        <ul>
+        <li>Szczupak, wystepowanie: stawy, rzeki</li>
+        <li>Sandacz, wystepowanie: stawy, jeziora, rzeki</li>
+        <li>Okon, wystepowanie: rzeki</li>
+        <li>Sum, wystepowanie:jeziora, rzeki</li>
+        <li>Dorsz, wystepowanie: morza, oceany</li>
+        <?php
+        ?>
+        </ul>
+    </div>
+    <div id="prawy">    
+        <img src="ryba1.jpg" alt="sum">
+        <br>
+        <a href="kwerendy.txt">Pobierz kwerendy</a>
+
+    </div>
+    <footer>
+    <p>Strone wykonal: 01270606398</p>
+    </footer>
+
+     
+</body>
+</html>
